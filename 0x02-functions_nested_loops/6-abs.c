9f5e15359a1bf2fb0592a7d6785db0a8e47e7aa9
Oyinade16 <109605435+Oyinade16@users.noreply.github.com>
2022-09-13 23:25:28 +0100

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,11 +3,11 @@
 /**
 * _abs - function that prints a absolute value of an integer
 *@c: takes in an integer
-* Return: 1 if greater than 0, 0 if zero and -1 if less than 0
+* Return: 0
  */
-int _abs(int n)
+int _abs(int c)
 {
-if (n < 0)
-return (n * -1);
-return (n);
+if (c < 0)
+return (c * -1);
+return (c);
 }
