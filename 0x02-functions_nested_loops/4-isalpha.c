2864da9475528b756c8bb8ebdd648e91691fd7c6
Oyinade16 <109605435+Oyinade16@users.noreply.github.com>
2022-09-13 23:12:02 +0100

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,11 +5,11 @@
 *@c: takes in a character
 *
 *Return: 1 if c is letter, lowercase or uppercase otherwise 0
- */
+*/
 int _isalpha(int c)
 {
- if ((c >= 'a' && c<= 'z') || (c >= 'A' && c<= 'Z'))
- return (1);
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+return (1);
 else
 return (0);
 }
