5d353fb2dec7c48fb48946b896dcf1676cdb7251
Oyinade16 <109605435+Oyinade16@users.noreply.github.com>
2022-09-15 12:42:23 +0100

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -8,22 +8,21 @@
  * Return: largest number
  */
 
-int largest_number(int a, int b, int c)
+intlargest_number(int a, int b, int c)
+{int largest;
+
+if (a > b && a > c)
 {
-	int largest;
+largest = a;
+}
+else if (b > c)
+{
+largest = b;
+}
+else
+{
+largest = c;
+}
 
-	if (a >= b && a >= c)
-	{
-		largest = a;
-	}
-	else if (b >= a && b >= c)
-	{
-		largest = b;
-	}
-	else
-	{
-		largest = c;
-	}
-
-	return (largest);
+return (largest);
 }
