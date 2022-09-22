dea841bfca0e3e34d80977422e48622c41f32987
Oyinade16 <109605435+Oyinade16@users.noreply.github.com>
2022-09-22 10:51:39 +0100

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,31 +1,31 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
-  * rot13 - ...
-  * @s: ...
-  *
-  * Return: ...
-  */
+ * rot13 - encodes a string into rot13
+ * @s: string to encode
+ *
+ * Return: address of s
+ */
+
 char *rot13(char *s)
+
 {
-        int a = 0;
+	int i, j;
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-        while (s[a])
-        {
-                while ((s[a] >= 'a' && s[a] <= 'z') || (s[a] >= 'A' && s[a] <= 'Z'))
-                {
-                        if ((s[a] > 'm' && s[a] <= 'z') || (s[a] > 'M' && s[a] <= 'Z'))
-                        {
-                                s[a] -= 13;
-                                break;
-                        }
+	for (i = 0; *(s + i); i++)
+{
+	
+	for (j = 0; j < 52; j++)
+{
+											if (a[j] == *(s + i))
+										{
 
-                        s[a] += 13;
-                        break;
-                }
-
-                a++;
-        }
-
-        return (s);
+	*(s + i) = b[j];
+											break;
+										}
+}
+										}
+	return (s);
+}
