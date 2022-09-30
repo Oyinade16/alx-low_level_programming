6aa4d1e6ae0b831a9546a185f3c838084c716c34
Oyinade16 <oyeniranoyinade16@gmail.com>
2022-09-30 15:25:18 -0700

    stac

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-gcc -c *.c
-ar -rcs liball.a *.o
-<<<<<<< HEAD
-=======
-ranlib liball.a
->>>>>>> 8fa28d10bf3d4a33c192082ab94c6bd0841963c9
+gcc -Wall -Wextra -Werror -pedantic -c *.c
+ar rc liball.a *.o
