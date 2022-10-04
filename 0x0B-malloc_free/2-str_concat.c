ed126f9bfc2d3294ef2c2fe82d73f544d891d27d
Oyinade16 <109605435+Oyinade16@users.noreply.github.com>
2022-10-04 21:24:55 +0100

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,48 +1,40 @@
+#include "main.h"
 #include <stdlib.h>
 
 /**
- * str_concat - a function that concatenates two strings.
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
  *
- * @s1: input to string 1
- * @s2: input to string 2
- *
- * Return: NULL on faliure
-*/
-
+ * Return: pointer to newly allocated space in memory, or NULL if error
+ */
 char *str_concat(char *s1, char *s2)
 {
-	int a = 0, b = 0;
-	int i, j;
+	unsigned int i, j, k, l;
 	char *s;
 
 	if (s1 == NULL)
-		s1 = "";
+		i = 0;
+	else
+	{
+		for (i = 0; s1[i]; i++)
+			;
+	}
 	if (s2 == NULL)
-		s2 = "";
-
-	/*find length of str1 & str2*/
-	while (s1[a] != '\0')
-		a++;
-	while (s2[b] != '\0')
-		b++;
-
-	/*+1 for our end of string character*/
-	s = malloc((a * sizeof(char)) + ((b + 1) * sizeof(char)));
-
+		j = 0;
+	else
+	{
+		for (j = 0; s2[j]; j++)
+			;
+	}
+	k = i + j + 1;
+	s = malloc(k * sizeof(char));
 	if (s == NULL)
 		return (NULL);
-
-	/*add the first string to array s*/
-	for (i = 0; s1[i] != '\0'; i++)
-		s[i] = s1[i];
-	/*add the second string to array s*/
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-		s[i] = s2[j];
-		i++;
-	}
-
-	/*null terminate our new string*/
-	s[i] = '\0';
-
+	for (l = 0; l < i; l++)
+		s[l] = s1[l];
+	for (l = 0; l < j; l++)
+		s[l + i] = s2[l];
+	s[i + j] = '\0';
 	return (s);
+}
